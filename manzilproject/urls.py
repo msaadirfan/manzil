e248f92a65a877b6e1969c8aca978179e7778b2f
msaadirfan <msaadirfan04@gmail.com>
2026-06-20 22:21:29 +0500

    Debugging

--- manzilproject/urls.py
+++ manzilproject/urls.py
@@ -14,6 +14,7 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from routefinder import views
 from django.contrib import admin
 from django.urls import path, include
 admin.site.site_header = "Manzil Admin"
@@ -24,4 +25,6 @@ admin.site.index_title = "Welcome to Manzil Admin Portal"
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('routefinder.urls')),
+    path('debug/', views.debug_station),
+
 ]
