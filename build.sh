91d5d569ad2df2facc24280f4a3067355e34ffab
Saad Irfan <msaadirfan04@gmail.com>
2026-06-20 21:34:14 +0500

    Refactor build.sh for production settings
    Updated the shebang and modified the collectstatic command to specify production settings.

--- build.sh
+++ build.sh
@@ -1,8 +1,2 @@
-#!/usr/bin/env bash
-# exit on error
-set -o errexit
-
-pip install -r requirements-prod.txt
-
-python manage.py collectstatic --no-input
-python manage.py migrate
+#!/bin/bash
+python manage.py collectstatic --no-input --settings=manzilproject.settings_production
